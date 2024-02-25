b39a4f86167b507cc2b0de995094ffa1185d709a
sireeshathumpati9 <sireeshathumpati9@gmail.com>
2024-02-25 23:00:06 +0000

    added 3 new ML Model code

--- model.py
+++ model.py
@@ -1,1 +1,10 @@
 print("adding all ML related code here")
+
+def model1():
+    print("adding code for model1")
+
+def model2():
+    print("adding code for model2")
+
+def model3():
+    print("adding code for model3")
